58a3b850af068a8ea0157dbf1e149fcab1026b1c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 16:45:50 -0300

    first commit

---
+++ src/test/java/com/exemplo/springboot_jpa_workshop/SpringbootJpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.springboot_jpa_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
