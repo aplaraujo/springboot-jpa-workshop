7d32a73e3b092bd9edc8602b6f04282641ece7dd
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 20:25:03 -0300

    Order, Instant, ISO 8601 part 2

---
+++ src/main/java/com/exemplo/springboot_jpa_workshop/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.exemplo.springboot_jpa_workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exemplo.springboot_jpa_workshop.entities.Order;
+import com.exemplo.springboot_jpa_workshop.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = orderRepository.findById(id);
+        return obj.get();
+    }
+}
